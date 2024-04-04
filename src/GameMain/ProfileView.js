086220aa21024841f5cbf021ad0a4be7cd7ea889
rushitv7265800 <107460866+rushitv7265800@users.noreply.github.com>
2024-04-04 18:25:10 +0000

    some c

--- src/GameMain/ProfileView.js
+++ src/GameMain/ProfileView.js
@@ -12,11 +12,11 @@ export default function ProfileView(props) {
     return (
         <>
             {
-                true && (
+                open && (
                     <div className="model profileView">
                         <div className="modelMain">
                             <div className='modelHead'>
-                                <h6>Promotion</h6>
+                                <h6>Profile</h6>
                                 <div className="closeIcon" onClick={() => setOpen(false)}>
                                     <img src={CloseIcon} />
                                 </div>
@@ -25,20 +25,64 @@ export default function ProfileView(props) {
                                 <div className='row'>
                                     <div className='col-6'>
                                         <div className='profileEdit'>
-                                            <div className='showImg'>
-                                                <img src={Avatar} />
-                                                <i class="bi bi-pencil-fill"></i>                                                                                                   
+                                            <div className='profileDetialsShow'>
+                                                <div className='showImg'>
+                                                    <img src={Avatar} />
+                                                    <i class="bi bi-pencil-fill"></i>
+                                                </div>
+                                                <div className='profileDetails'>
+                                                    <h6>Player</h6>
+                                                    <h6>+91 1234567890</h6>
+                                                </div>
+                                            </div>
+                                            <div className="profileAvtarProfile">
+                                                <div className='showImg'>
+                                                    <img src={Avatar1} />
+                                                </div>
+                                                <div className='showImg'>
+                                                    <img src={Avatar} />
+                                                </div>
+                                                <div className='showImg'>
+                                                    <img src={Avatar3} />
+                                                </div>
+                                                <div className='showImg'>
+                                                    <img src={Avatar4} />
+                                                </div>
+                                                <div className='showImg'>
+                                                    <img src={Avatar5} />
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
                                 <div className='col-6'>
+                                    <form>
+                                    <div className='inputProfileBox'>
+                                        <div className='inputShowProfile'>
+                                            <label>User Name</label>
+                                            <input type='text' />
+                                        </div>
+                                        <div className='inputShowProfile'>
+                                            <label>Email</label>
+                                            <input type='text' />
+                                        </div>
+                                        <div className='inputShowProfile'>
+                                            <label>Phone No.</label>
+                                            <input type='number' />
+                                        </div>
+                                        <div className='inputShowProfile'>
+                                            <label>Password</label>
+                                            <input type='number' />
+                                        </div>
+                                    </div>
+                                    <button>Submit</button>
+                                    </form>
+                                </div>
                                 </div>
                             </div>
                         </div>
                     </div >
                 )
-}
+            }
         </>
     )
 }
